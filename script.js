c5c673b71a29fa44d920ef7f701c34cc47138093
Jeromy Schneider <JeromySchneider@live.com>
2022-07-11 08:14:46 -0700

    feat: Add functionality to create a new custom size grid
    The squares scale to the fixed dimensions of the grid.

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const canvas = document.getElementById("canvas");
 
-let canvasSize = 16**2;
-for (let i = 0; i < canvasSize; i++) {
-  let square = document.createElement("div");
-  canvas.appendChild(square);
+function createGrid(num) {
+  for (let i = 0; i < num; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    canvas.appendChild(row);
+    for (let i = 0; i < num; i++) {
+      let square = document.createElement("div");
+      row.appendChild(square);
+    }
+  }
+  const grid = document.querySelectorAll(".row > div");
+  grid.forEach(square => {
+    square.addEventListener("mouseover", (e) => {
+      e.target.style.background = "lightskyblue";
+    });
+  });
 }
 
-const grid = document.querySelectorAll("div#canvas > div");
-
-grid.forEach((square) => {
-  square.addEventListener("mouseover", (s) => {
-    s.target.style.background = "lightskyblue";
+function removeGrid() {
+  const grid = document.querySelectorAll(".row");
+  grid.forEach(square => {
+    square.remove();
   });
+}
+
+const newGrid = document.getElementById("gridSize");
+
+newGrid.addEventListener("click", () => {
+  let input = prompt("Grid size: ");
+  if ((input > 0) && (input <= 100)) {
+    removeGrid();
+    createGrid(input);
+  }
 });
+
+const defaultGridSize = 8;
+createGrid(defaultGridSize);
