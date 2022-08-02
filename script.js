c6f088240956674a16aa9760ab8eb76b3b9723f0
Jeromy Schneider <JeromySchneider@live.com>
2022-08-02 01:23:38 -0700

    feat: Add color input to select color and a button to toggle rainbow colors

--- script.js
+++ script.js
@@ -10,27 +10,59 @@ function createGrid(num) {
       row.appendChild(square);
     }
   }
-  const grid = document.querySelectorAll(".row > div");
-  grid.forEach(square => {
-    square.addEventListener("mouseover", (e) => {
+  
+  function removeGrid() {
+    const grid = document.querySelectorAll(".row");
+    grid.forEach(square => {
+      square.remove();
+    });
+  }
+  
+  function getColor() {
+    if (rainbowToggle) {
       let r = getRandomInt(0, 255);
       let g = getRandomInt(0, 255);
       let b = getRandomInt(0, 255);
-      e.target.style.background = `rgb(${r},${g},${b})`;
+      return `rgb(${r},${g},${b})`;
+    } else {
+      return colorOfSquare;
+    }
+  }
+  
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const grid = document.querySelectorAll(".row > div");
+  
+  grid.forEach(square => {
+    square.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = getColor();
     });
   });
 }
 
-function removeGrid() {
-  const grid = document.querySelectorAll(".row");
-  grid.forEach(square => {
-    square.remove();
-  });
-}
+const color = document.getElementById("color");
+let colorOfSquare = color.value;
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+color.addEventListener("input", () => {
+  colorOfSquare = color.value;
+});
+
+const rainbow = document.getElementById("rainbow");
+let rainbowToggle = false;
+
+rainbow.addEventListener("mousedown", () => {
+  if (rainbowToggle) {
+    rainbowToggle = false;
+    rainbow.style.color = "black";
+    rainbow.style.backgroundColor = "white";
+  } else {
+    rainbowToggle = true;
+    rainbow.style.color = "white";
+    rainbow.style.backgroundColor = "black";
+  }
+});
 
 const slider = document.getElementById("slider");
 let sliderInput = slider.value;
